d763f3324c8f7d70ed54e1e39e8d0ef82f3590c5
towyuan <towyuan@outlook.com>
2018-01-09 17:56:35 +0800

    Add missing initializers phase

---
+++ test/phases/initializers.test.js
@@ -0,0 +1,109 @@
+/* global describe, it, before, expect */
+
+const initializers = require('../../lib/phases/initializers');
+
+describe('phases/initializers', function() {
+
+    it('should export a setup function', function() {
+        expect(initializers).to.be.a('function');
+    });
+
+    describe('phase with string argument', function() {
+        const app = new Object();
+        app.order = [];
+
+        let error;
+
+        before(function(done) {
+            global.__app = app;
+
+            const phase = initializers(__dirname + '/../data/initializers/test');
+            phase.call(app, function(err) {
+                error = err;
+                delete global.__app;
+                return done();
+            });
+        });
+
+        it('should call callback', function() {
+            expect(error).to.be.undefined;
+        });
+        it('should run initializers in correct order', function() {
+            expect(app.order).to.have.length(3);
+            expect(app.order[0]).to.equal('01_async');
+            expect(app.order[1]).to.equal('02_sync');
+            expect(app.order[2]).to.equal('03_require');
+        });
+    });
+
+    describe('phase with dirname option', function() {
+        const app = new Object();
+        app.order = [];
+
+        let error;
+
+        before(function(done) {
+            const phase = initializers({ dirname: __dirname + '/../data/initializers/test' });
+            phase.call(app, function(err) {
+                error = err;
+                return done();
+            });
+        });
+
+        it('should call callback', function() {
+            expect(error).to.be.undefined;
+        });
+        it('should run initializers in correct order', function() {
+            expect(app.order).to.have.length(2);
+            expect(app.order[0]).to.equal('01_async');
+            expect(app.order[1]).to.equal('02_sync');
+        });
+    });
+
+    describe('phase with initializer that calls done with error', function() {
+        const app = new Object();
+        app.order = [];
+
+        let error;
+
+        before(function(done) {
+            const phase = initializers(__dirname + '/../data/initializers/error-done');
+            phase.call(app, function(err) {
+                error = err;
+                return done();
+            });
+        });
+
+        it('should call callback', function() {
+            expect(error).to.be.an.instanceOf(Error);
+            expect(error.message).to.equal('something went wrong');
+        });
+        it('should halt initializers after error', function() {
+            expect(app.order).to.have.length(0);
+        });
+    });
+
+    describe('phase with initializer that throws exception', function() {
+        const app = new Object();
+        app.order = [];
+
+        let error;
+
+        before(function(done) {
+            const phase = initializers(__dirname + '/../data/initializers/error-throw');
+            phase.call(app, function(err) {
+                error = err;
+                return done();
+            });
+        });
+
+        it('should call callback', function() {
+            expect(error).to.be.an.instanceOf(Error);
+            expect(error.message).to.equal('something went horribly wrong');
+        });
+        it('should halt initializers after error', function() {
+            expect(app.order).to.have.length(0);
+        });
+    });
+
+});
